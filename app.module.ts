d9a6128958d9d61c87a7cd44f0a0971804ec25e7
ASHLY BADGETT <ashlybadgett@gmail.com>
2017-08-29 18:36:09 -0500

    feat(analytics): added google analytics

--- app.module.ts
+++ app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { MapComponent } from './map/map.component';
+import { FooterComponent } from './src/app/footer/footer.component';
+import { HeaderComponent } from './src/app/header/header.component';
+import { MapComponent } from './src/app/map/map.component';
+import { ContentComponent } from './src/app/content/content.component';
 
 import { AgmCoreModule } from '@agm/core';
 import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
-import { ContentComponent } from './content/content.component';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
 
 @NgModule({
@@ -21,9 +21,9 @@ import { Ng2PageScrollModule } from 'ng2-page-scroll';
   ],
   imports: [
     BrowserModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyACBE0zHMOsSJxQZ_IQRF1BWijPYdAHxp4'
-    }),
+    AgmCoreModule.forRoot(
+      {apiKey: 'AIzaSyACBE0zHMOsSJxQZ_IQRF1BWijPYdAHxp4'}
+    ),
     AgmSnazzyInfoWindowModule,
     Ng2PageScrollModule
   ],
