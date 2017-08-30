b1b55003680ebc7323f67a696071f2cb4e4ac75f
ASHLY BADGETT <ashlybadgett@gmail.com>
2017-08-30 00:01:38 -0500

    fix(server): reverting changes

--- server.js
+++ server.js
@@ -1,42 +1,14 @@
-const http = require('http');
-const https = require('https');
 const express = require('express');
-const fs = require('fs');
-const morgan = require('morgan');
-const config = require('./tsconfig.json');
+const app = express();
 
-var options = {
-    key: fs.readFileSync('./ssl/key.pem'),
-    cert: fs.readFileSync('./ssl/cert.pem')
-};
-
-let app = express();
-
-app.server = https.createServer(options, app);
-
-app.use(morgan('dev'));
+// Run the app by serving the static files
+// in the dist directory
 
 app.use(express.static(__dirname + '/dist'));
+console.log(__dirname + '/dist');
 
-console.log(process.env.PORT)
-console.log(config.port)
+// Start the app by listening on the default
+// Heroku port
 
-app.server.listen(process.env.PORT || config.port, () => {
-    console.log(`Started on port ${app.server.address().port}`);
-});
-
-
-// const express = require('express');
-// const app = express();
-
-// // Run the app by serving the static files
-// // in the dist directory
-
-// app.use(express.static(__dirname + '/dist'));
-// console.log(__dirname + '/dist');
-
-// // Start the app by listening on the default
-// // Heroku port
-
-// app.listen(process.env.PORT || 8080);
-// console.log(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
+console.log(process.env.PORT || 8080);
