911dd127868d958657d1a31fc46f0924acfcfa5c
ASHLY BADGETT <ashlybadgett@gmail.com>
2017-09-26 09:53:19 -0500

    fix(site): added meta description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AgmCoreModule } from '@agm/core';
 import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 import { ContentComponent } from './content/content.component';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { Meta } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -30,4 +31,8 @@ import { Ng2PageScrollModule } from 'ng2-page-scroll';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private metaService: Meta) {
+    this.metaService.addTag({ property: 'description', content: 'Cadet Cleaners is a family owned dry cleaners that has been serving the Dallas and Duncanville areas for over 30 years. We pride ourselves on our attention to detail, as well as giving our customers the best possible customer service.' });
+  }
+}
